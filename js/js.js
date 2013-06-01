1519c6bf9323b0224bbc6ef784bff7cda317e502
Tom Christilaw <tomchristilaw@student.cascadia.edu>
2013-06-01 00:54:17 -0700

    revert more more more

--- js/js.js
+++ js/js.js
@@ -1,21 +1,19 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-
-function onDeviceReady() {	
-		$('#categories .icons:first-of-type figure').addClass('visible');
-		$('.icons figure').click(function() {
-			if (!$(this).hasClass('selected')) {		
-				if ($(this).hasClass('care') || $(this).hasClass('upholstery')) {
-					$(this).addClass('selected')
-						.next().removeClass('visible');
-				} else {
-					$(this).addClass('selected')
-						.prev().removeClass('visible');
-				}
-				if ($(this).parent().hasClass('topIconBox')) {
-					$('.subIconBox figure').addClass('visible');
-				}
-				$(this).parent().css('display', 'inline-block')
-					.css('width', '125px');	
+$(document).ready(function() {
+	$('#categories .icons:first-of-type figure').addClass('visible');
+	$('.icons figure').click(function() {
+		if (!$(this).hasClass('selected')) {		
+			if ($(this).hasClass('care') || $(this).hasClass('upholstery')) {
+				$(this).addClass('selected')
+					.next().removeClass('visible');
+			} else {
+				$(this).addClass('selected')
+					.prev().removeClass('visible');
 			}
+			if ($(this).parent().hasClass('topIconBox')) {
+				$('.subIconBox figure').addClass('visible');
+			}
+			$(this).parent().css('display', 'inline-block')
+				.css('width', '125px');	
+		}	
 	});
-}
+});
