ab8d1d3c2703f9c1e5d936f9e6e2aecfdcb2e37b
Tom Christilaw <tomchristilaw@student.cascadia.edu>
2013-06-01 00:03:53 -0700

    rebuilt categories page

---
+++ js/js.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('#categories .icons:first-of-type figure').addClass('visible');
+	$('.icons figure').click(function() {		
+		if ($(this).hasClass('care') || $(this).hasClass('upholstery')) {
+			$(this).addClass('selected')
+				.next().removeClass('visible');
+		} else {
+			$(this).addClass('selected')
+				.prev().removeClass('visible');
+		}
+		if ($(this).parent().hasClass('topIconBox')) {
+			$('.subIconBox figure').addClass('visible');
+		}
+		$(this).parent().css('display', 'inline-block')
+			.css('width', '125px');		
+	});
+});
