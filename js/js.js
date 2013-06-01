fe738d303a4e131ef4425f52b2f4c25bd5666fd1
Tom Christilaw <tomchristilaw@student.cascadia.edu>
2013-06-01 00:28:18 -0700

    edited function

--- js/js.js
+++ js/js.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
 	$('#categories .icons:first-of-type figure').addClass('visible');
-	$('.icons figure').click(function() {		
-		if ($(this).hasClass('care') || $(this).hasClass('upholstery')) {
-			$(this).addClass('selected')
-				.next().removeClass('visible');
-		} else {
-			$(this).addClass('selected')
-				.prev().removeClass('visible');
-		}
-		if ($(this).parent().hasClass('topIconBox')) {
-			$('.subIconBox figure').addClass('visible');
-		}
-		$(this).parent().css('display', 'inline-block')
-			.css('width', '125px');		
+	$('.icons figure').click(function() {
+		if (!$(this).hasClass('selected')) {		
+			if ($(this).hasClass('care') || $(this).hasClass('upholstery')) {
+				$(this).addClass('selected')
+					.next().removeClass('visible');
+			} else {
+				$(this).addClass('selected')
+					.prev().removeClass('visible');
+			}
+			if ($(this).parent().hasClass('topIconBox')) {
+				$('.subIconBox figure').addClass('visible');
+			}
+			$(this).parent().css('display', 'inline-block')
+				.css('width', '125px');	
+		}	
 	});
 });
